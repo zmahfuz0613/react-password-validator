574b2692fd7efd90cb0111f86c0d0e157fcb5cc7
zmahfuz061318 <str053mgr@gmail.com>
2020-07-22 15:46:51 -0400

    Work on progress

--- src/Validator.js
+++ src/Validator.js
@@ -5,8 +5,16 @@ function Validator() {
   return (
     <div className="form">
       <h1>Sign Up</h1>
-      <input type="text" placeholder="username" />
+      <input type="text" value={this.state.input.email}
+              onChange={this.handleChange}
+              class="form-control" 
+              placeholder="Enter email" 
+              id="email" /> />
       <input type="password" placeholder="password" />
+      <input type="password" placeholder="confirm password" />
+      <input type="submit" value="Submit" class="btn btn-success" />
+
+
     </div>
   );
 }
